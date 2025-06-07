1ce68eb29a2879cdaa92aa69c8ba8bf00f2582a9
MrinalS24 <connectwithmrinaal@gmail.com>
2025-06-07 11:57:17 +0200

    Add files via upload

--- Quiz Game/main.py
+++ Quiz Game/main.py
@@ -5,7 +5,7 @@ HEIGHT = 650
 
 TITLE = "Quiz Game"
 
-marquee_box = Rect(0,0,880,80)
+marquee_box = Rect(0,0,1050,80)
 question_box = Rect(0,0,650,150)
 timer_box = Rect(0,0, 150, 150)
 answer_box1 = Rect(0,0, 300, 150)
@@ -24,9 +24,20 @@ skip_box.move_ip(700,270)
 
 time_left = 10
 
+question_index = 0
+question_count = 0
+questions = []
+score = 0
+game_over = False
+
+marquee_message = ""
+
+
+
 answers_boxes = [answer_box1, answer_box2, answer_box3, answer_box4]
 
 def draw():
+    global marquee_message, question
     screen.clear()
     screen.fill(color= "black")
     screen.draw.filled_rect(marquee_box, "black")
@@ -37,6 +48,50 @@ def draw():
         screen.draw.filled_rect(i, "pink")
     screen.draw.textbox(str(time_left), timer_box, color = "white")
     screen.draw.textbox("Skip", skip_box, color = "white")
+
+    marquee_message =f"Welcome to Quiz Game. You are at question {question_index} out of {question_count}"
+    screen.draw.textbox(marquee_message, marquee_box, color = "white")
+
+    screen.draw.textbox(question[0].strip(), question_box, color = "black")
+
+
+def marquee_effect():
+    marquee_box.x -= 1
+    if marquee_box.right < 0:
+        marquee_box.left = WIDTH
+
+
+def update():
+    marquee_effect()
+
+
+
+def read_question_file():
+    global questions, question_count
+    q_file = open("questions.txt", "r")
+    for row in q_file:
+        questions.append(row)
+        question_count += 1
+
+
+def read_question():
+    global question_index
+    question_index += 1
+    question_option = questions.pop(0).split(",")
+    return(question_option)
+
+
+
+
+
+
+
+read_question_file()
+
+
+question = read_question()
+
+
 
 
 
@@ -62,3 +117,8 @@ def draw():
 
 
 pgzrun.go()
+
+
+
+
+#strip removes /n and spaces before and after the string
